8d154f42e8f16e6483d85aa8e149dd3317487876
FattiMei <teomei68@gmail.com>
2023-04-02 09:50:50 +0200

    Isolato solo la funzionalità essenziale

--- Main.hs
+++ Main.hs
@@ -12,7 +12,6 @@ type Seconds = Float
 type Samples = Float
 type Hz = Float
 type Semitones = Float
-type Beats = Float
 
 outputFilePath :: FilePath
 outputFilePath = "output.bin"
@@ -26,18 +25,10 @@ sampleRate = 48000.0
 pitchStandard :: Hz
 pitchStandard = 440.0
 
-bpm :: Beats
-bpm = 120.0
-
-beatDuration :: Seconds
-beatDuration = 60.0 / bpm
 
 -- NOTE: the formula is taken from https://pages.mtu.edu/~suits/NoteFreqCalcs.html
 f :: Semitones -> Hz
 f n = pitchStandard * (2 ** (1.0 / 12.0)) ** n
-
-note :: Semitones -> Beats -> [Pulse]
-note n beats = freq (f n) (beats * beatDuration)
 
 freq :: Hz -> Seconds -> [Pulse]
 freq hz duration =
@@ -54,58 +45,14 @@ freq hz duration =
     output :: [Pulse]
     output = map sin $ map (* step) [0.0 .. sampleRate * duration]
 
-wave :: [Pulse]
-wave =
-  concat
-    [ note 0 0.25
-    , note 0 0.25
-    , note 0 0.25
-    , note 0 0.25
-    , note 0 0.5
-    , note 0 0.25
-    , note 0 0.25
-    , note 0 0.25
-    , note 0 0.25
-    , note 0 0.25
-    , note 0 0.25
-    , note 0 0.5
-    , note 5 0.25
-    , note 5 0.25
-    , note 5 0.25
-    , note 5 0.25
-    , note 5 0.25
-    , note 5 0.25
-    , note 5 0.5
-    , note 3 0.25
-    , note 3 0.25
-    , note 3 0.25
-    , note 3 0.25
-    , note 3 0.25
-    , note 3 0.25
-    , note 3 0.5
-    , note (-2) 0.5
-    , note 0 0.25
-    , note 0 0.25
-    , note 0 0.25
-    , note 0 0.25
-    , note 0 0.5
-    ]
-
-hehehe :: [Pulse]
-hehehe = concat [ note 0 0.25
-                , note 0 0.25
-                , note 12 0.5
-                , note 7 (0.5 + 0.25)
-                , note 6 0.5
-                , note 5 0.5
-                , note 3 0.5
-                , note 0 0.25
-                , note 3 0.25
-                , note 5 0.25
-                ]
+arpeggio :: [Pulse]
+arpeggio = concat [ freq pitchStandard 2.0
+                  , freq (pitchStandard * (5.0 / 4.0)) 2.0
+				  , freq (pitchStandard * 1.5) 2.0
+				  ]
 
 save :: FilePath -> IO ()
-save filePath = B.writeFile filePath $ B.toLazyByteString $ fold $ map B.floatLE hehehe
+save filePath = B.writeFile filePath $ B.toLazyByteString $ fold $ map B.floatLE arpeggio
 
 play :: IO ()
 play = do
